5d46dce59bd696260e7624bf00d79758c3690419
Mark Yoo <yjinhyuk@gmail.com>
2021-05-09 21:09:52 +0900

    Add: copyPreventModal is now added Detail: copyPreventModal can prevent user from dragging texts for unauthorized copy

---
+++ src/components/copyPreventModal.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./scss/copyPreventModal.scss";
+
+// 사용할 부모 요소에 position: relative 적용 후 사용
+function CopyPreventModal() {
+  return <div className="copy_prevent"></div>;
+}
+
+export default CopyPreventModal;
